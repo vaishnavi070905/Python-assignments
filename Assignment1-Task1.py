40d1b49f756ff11a75d877bc73622ec6082bcd63
vaishnavi070905 <vaishnaving07@gmail.com>
2025-08-04 00:12:15 +0530

    Create Assignment1-Task1.py

---
+++ Assignment1-Task1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+
+print("Addition : ", c)
+print("Subtraction : ",d)
+print("Multiplication : ",e)
+print("Division : ",f)

435284c2fad4857758e785d5ee9c8963ded72bf0
vaishnavi070905 <vaishnaving07@gmail.com>
2025-08-05 20:44:38 +0530

    Create Assignment5-Task2.py

---
+++ Assignment5-Task2.py
@@ -0,0 +1,11 @@
+# Create list from 1 to 10
+numbers = list(range(1, 11))
+print("Original list:", numbers)
+
+# Extracting first five elements
+first_five = numbers[:5]
+print("Extracted first five elements:", first_five)
+
+# Reversing extracted elements
+reversed_first_five = first_five[::-1]
+print("Reversed extracted elements:", reversed_first_five)
